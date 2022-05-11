f4f07cd5e0124694417a6048fb2ab01975eedcc1
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2022-05-11 23:28:51 +0530

    Update and rename Find intersection point of Y LinkedList .cpp to Find intersection point of two LinkedList .cpp

--- Find intersection point of two LinkedList .cpp
+++ Find intersection point of two LinkedList .cpp
@@ -36,6 +36,14 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     }
 };
 
+// I will be initializing two pointers a and b  at the headA and  headB.
+// after that will traverse through each the lists,one node at a time.
+// When poinyer -a reaches the end of a list, then we need to redirect it to the headB.
+// similarly when pointer -b reaches the end of a list, redirect it the headB.
+// this will make both the pointer a nd b now be at equal distance from each , after this when they will we move will easily detect the intersection point.
+// mow at any node a meets b, then it is the intersection node.
+// And if there is no intersection node it returns NULL.
+
 // T.C. : o(n) and S.C. : o(1)
 /**
  * Definition for singly-linked list.
