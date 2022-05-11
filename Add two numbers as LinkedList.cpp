d58068432c0e0781d0b493ce7b4fab6815f86678
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2022-05-11 23:14:50 +0530

    Update Add two numbers as LinkedList.cpp

--- Add two numbers as LinkedList.cpp
+++ Add two numbers as LinkedList.cpp
@@ -1,3 +1,15 @@
+//Create a dummy node which is the head of new linked list.
+// Create a node temp, initialise it with dummy.
+// Initialize carry to 0.
+// Loop through lists l1 and l2 until you reach both ends, and until carry is present.
+// Set sum=l1.val+ l2.val + carry.
+// Update carry=sum/10.
+// Create a new node with the digit value of (sum%10) and set it to temp node’s next, then advance temp node to next.
+// Advance both l1 and l2.
+// Return dummy’s next node.
+// Note that we use a dummy head to simplify the code. Without a dummy head, you would have to write extra conditional statements to initialize the head’s value.
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
